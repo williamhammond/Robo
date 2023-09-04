0d42c1b783b1aca53e7db5dc0efc000819a210fd
William Hammond <william.t.hammond@gmail.com>
2023-09-04 16:26:48 -0400

    Adds ServerLib

--- test/IntegrationTests.cpp
+++ test/IntegrationTests.cpp
@@ -1,6 +1,35 @@
 #include <gtest/gtest.h>
 
-TEST(WhamTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+#include "Server.h"
+
+class IntegrationTests : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    Server::StaticInit();
+    serverThread = std::thread([this]() {
+      serverInitializationCV.notify_one();
+      serverReady = true;
+      // TODO @WilliamHammond Probably a race condition. We need to add something like an IsReady flag
+      // to the engine
+      Server::Instance->Run();
+    });
+    std::unique_lock<std::mutex> lock(mutex);
+    serverInitializationCV.wait(lock, [this]() { return serverReady; });
+  }
+
+  void TearDown() override {
+    Server::Instance->SetQuit(true);
+    if (serverThread.joinable()) {
+      serverThread.join();
+    }
+  }
+
+  std::condition_variable serverInitializationCV;
+  std::mutex mutex;
+  std::thread serverThread;
+  bool serverReady = false;
+};
+
+TEST_F(IntegrationTests, BasicAssertions) {
+  ASSERT_TRUE(true);
 }
